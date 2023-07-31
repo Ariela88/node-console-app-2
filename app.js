05ae68b84e042fa07c42e1c825db5240de29e97b
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-31 11:58:58 +0200

    node task

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const fs = require("fs");
-const transformCsvToJson = require('./json-converter');
-const transformJsonToCsv = require('./csv-converter');
+// const transformCsvToJson = require('./json-converter');
+// const transformJsonToCsv = require('./csv-converter');
 // const jsonConverter = require('./json-converter');
 // console.log(jsonConverter);
 
 const inputUrl = process.argv[2];
+const splittedInputUrl = inputUrl.split('.');
+const ext = splittedInputUrl[splittedInputUrl.length - 1];
+
+let transformFunction;
+if (ext.toLowerCase().includes('json')) {
+    transformFunction = require('./csv-converter');
+} else if (ext.toLowerCase().includes('csv')){
+    transformFunction = require('./json-converter');
+} else {
+    console.log('non posso convertire i file: ' + ext);
+    process.exit();
+}
+
+
 const outputUrl = process.argv[3];
+
 let divider = process.argv[4];
 if (divider === undefined) { //A2)aggiungere un parametro alla applicazione che mi permette di indicare il carattere divisorio
     divider = ',';
 }
+
 console.log(divider);
 
 let data = readFile(inputUrl)
 
 if (data) {
 
-    const result = transformJsonToCsv(data, divider)
+    const result = transformFunction(data, divider)
 
     writeData(outputUrl, result);
 }
